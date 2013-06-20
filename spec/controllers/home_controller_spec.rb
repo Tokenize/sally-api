9c44141492b56b9a0dd0720b7e4d4792e931c832
Mike Farr <mike@tokenize.ca>
2013-06-20 12:50:43 -0400

    Add home controller tests

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -4,8 +4,13 @@ describe HomeController do
 
   describe "GET 'index'" do
     it "returns http success" do
-      get 'index'
-      response.should be_success
+      get :index
+      expect(response).to be_success
+    end
+
+    it "renders the :index view" do
+      get :index
+      expect(response).to render_template :index
     end
   end
 
