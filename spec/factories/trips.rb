023eeb3b6defec04a502908720ec2b31fc9fa6e5
Mike Farr <mike@tokenize.ca>
2013-06-23 20:08:13 -0400

    Tweak Trip factory

--- spec/factories/trips.rb
+++ spec/factories/trips.rb
@@ -1,7 +1,7 @@
 require 'faker'
 FactoryGirl.define do
   factory :trip do
-    name { Faker::Lorem.words }
+    name { Faker::Company.bs }
     description { Faker::Lorem.paragraph }
     start_at { Time.now }
     end_at { Time.at(Time.now + rand(10000)) }
