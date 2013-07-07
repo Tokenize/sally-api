c6b68c14be1f59b5c60661ed5fbee2459f85fd33
Mike Farr <mike@tokenize.ca>
2013-07-07 08:15:19 -0400

    Implement trip retrieval by id, refactor trip tests.

--- app/api/sally/trips.rb
+++ app/api/sally/trips.rb
@@ -3,10 +3,22 @@ module Sally
   class Trips < Grape::API
     helpers AuthHelpers
 
+    before do
+      authenticated_user
+    end
+
     desc "Returns all trips."
     get :trips do
-      authenticated_user
       current_user.trips.to_a
     end
+
+    desc "Return a trip."
+    params do
+      requires :id, type: Integer, desc: "Trip id."
+    end
+    get "trips/:id" do
+      current_user.trips.where(id: params[:id]).first
+    end
+
   end
 end
--- spec/api/sally/trips_spec.rb
+++ spec/api/sally/trips_spec.rb
@@ -1,15 +1,36 @@
 require 'spec_helper'
-describe 'GET trips' do
+describe 'Sally::Trips' do
   before :each do
     @user = create(:user)
-    @trip = create(:trip, user: @user)
-    @token = @user.authentication_token
   end
-  
+
   context "authenticated user" do
+    before :each do
+      @trip1 = create(:trip, user: @user)
+      @trip2 = create(:trip, user: @user)
+      @token = @user.authentication_token
+    end
+
     it "responds with an 'OK (200)' status" do
       get 'api/trips', { auth_token: @token }
       expect(response.response_code).to eq 200
+    end
+
+    describe 'GET trips' do
+      it "returns the users trips" do
+        get 'api/trips', { auth_token: @token }
+        body = JSON.parse(response.body)
+        expect(body.count).to eq 2
+      end
+    end
+
+    describe 'GET trips/:id' do
+      it "returns a specific trip when given a trip id" do
+        get "api/trips/#{@trip1.id}", { auth_token: @token }
+        body = JSON.parse(response.body)
+        expect(body['id']).to eq @trip1.id
+      end
+
     end
   end
 
@@ -20,3 +41,4 @@ describe 'GET trips' do
     end 
   end
 end
+
