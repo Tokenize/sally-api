c84bc05a038c3780470fd05ce256218c5b649c50
Zaid Al-Jarrah <zaid.aljarrah@gmail.com>
2013-08-27 22:57:24 -0400

    Make 'trip_params' return 'params' if its empty
    This way we can use trip_params for all POST/GET/PUT/DELETE methods
    without having a mix of calls to params / trip_params.

--- app/api/sally/helpers/param_helpers.rb
+++ app/api/sally/helpers/param_helpers.rb
@@ -4,6 +4,6 @@ module ParamHelpers
   end
 
   def trip_params
-    clean_params.permit(:name, :description, :start_at, :end_at)
+    clean_params.permit(:name, :description, :start_at, :end_at).presence || params
   end
 end
